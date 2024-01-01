50d5e9b3ab124a24bd827c71eca9af811053a2ea
Justin Antranikian <justin.antranikian@gmail.com>
2023-12-31 20:04:55 -0700

    removed reporting code

--- Orchestration/Reports/AthleteResultsReport/AthleteReportDto.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Orchestration.Reports.AthleteResultsReport;
-
-public record AthleteReportDto(int AthleteId, string FirstName, string LastName, string FullName, List<AthleteReportCourseDto> AthleteReportCourseDtos);
-
-public record AthleteReportCourseDto(int CourseId, string CourseName, string RaceSeriesName, DateTime KickOffDate, double CourseDistance, RaceSeriesType RaceSeriesType);
--- Orchestration/Reports/RacesByMonthReport/ReportByMonthDto.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Orchestration.Reports.RacesByMonthReport;
-
-public record ReportByMonthRaceDto(int Id, string Name, DateTime KickOffDate, RaceSeriesType RaceSeriesType);
-
-public record ReportByMonthDto(string MonthName, List<ReportByMonthRaceDto> Races);
