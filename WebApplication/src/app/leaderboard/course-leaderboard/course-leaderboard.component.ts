71666b1279d23773aed27fe45fd7fc4ec4a1d7a0
Justin Antranikian <justin.antranikian@gmail.com>
2024-02-04 08:27:11 -0700

    changed to be regular bootstrap tables

--- WebApplication/src/app/leaderboard/course-leaderboard/course-leaderboard.component.ts
+++ WebApplication/src/app/leaderboard/course-leaderboard/course-leaderboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BracketType } from '../../_core/enums/bracketType';
 import { BreadcrumbLocation } from '../../_common/breadcrumbLocation';
