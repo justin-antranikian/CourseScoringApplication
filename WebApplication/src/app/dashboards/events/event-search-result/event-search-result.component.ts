c7d43ad934831b82be18c5644bb759ceabb0b071
Justin Antranikian <justin.antranikian@gmail.com>
2024-02-18 09:43:34 -0700

    small changes to event-search-result-component

--- WebApplication/src/app/dashboards/events/event-search-result/event-search-result.component.ts
+++ WebApplication/src/app/dashboards/events/event-search-result/event-search-result.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { tap } from 'rxjs/operators';
 import { EventSearchResultDto } from '../../../_core/eventSearchResultDto';
 import { ComponentBaseWithRoutes } from '../../../_common/componentBaseWithRoutes';
 import { CommonModule } from '@angular/common';
@@ -8,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { LocationInfoRankingsComponent } from '../../../_subComponents/location-info-rankings/location-info-rankings.component';
 import { LeaderboardResultComponent } from '../../../_subComponents/leaderboard-results-grid/leaderboard-result.component';
 import { ScoringApiService } from '../../../services/scoring-api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -16,7 +16,7 @@ import { ScoringApiService } from '../../../services/scoring-api.service';
   styleUrls: ['./event-search-result.component.css'],
   imports: [CommonModule, RouterModule, LocationInfoRankingsComponent, NgbModule, LeaderboardResultComponent]
 })
-export class EventSearchResultComponent extends ComponentBaseWithRoutes {
+export class EventSearchResultComponent extends ComponentBaseWithRoutes implements OnInit {
 
   constructor(
     private modalService: NgbModal,
@@ -26,40 +26,20 @@ export class EventSearchResultComponent extends ComponentBaseWithRoutes {
   @Input('eventSearchResult')
   public eventSearchResult!: EventSearchResultDto
 
-  // props for when view leaderboard is clicked.
-  public raceName!: string
-  public raceSeriesDescription!: string
-  public raceKickOffDate!: string
-  public locationInfoWithRank: any
-  public leaderboards!: any[]
+  public raceLeaderboard: any | null
+
+  public subscription: Subscription | null = null
+
+  ngOnInit() {
+    
+  }
 
   public onViewLeaderboardClicked = (modal: any) => {
     const raceId = this.eventSearchResult.upcomingRaceId
-    const getLeaderboard$ = this.scoringApiService.getRaceLeaderboard(raceId).pipe(
-      tap(this.setPropsToRender)
-    )
 
-    getLeaderboard$.subscribe((_raceLeaderboardDto: any) => {
+    this.scoringApiService.getRaceLeaderboard(raceId).subscribe((raceLeaderboardDto: any) => {
+      this.raceLeaderboard = raceLeaderboardDto
       this.modalService.open(modal, { size: 'xl' });
     });
-  }
-
-  /**
-  * this sets the properties of the RaceLeaderboardDto when user clicks to view more.
-  */
-  private setPropsToRender = (raceLeaderboardDto: any) => {
-    const {
-      raceName,
-      raceSeriesDescription,
-      raceKickOffDate,
-      locationInfoWithRank,
-      leaderboards,
-    } = raceLeaderboardDto
-
-    this.raceName = raceName
-    this.raceSeriesDescription = raceSeriesDescription
-    this.raceKickOffDate = raceKickOffDate
-    this.locationInfoWithRank = locationInfoWithRank
-    this.leaderboards = leaderboards
   }
 }
