be7031a41b669e54f274f1408498b9f374faa808
Justin Antranikian <justin.antranikian@gmail.com>
2024-12-02 19:39:31 -0700

    JA/added-sheet-slideout

--- nextwebapp/src/app/events/_components/QuickViewDialogContent.tsx
+++ nextwebapp/src/app/events/_components/QuickViewDialogContent.tsx
@@ -4,12 +4,14 @@ import { RaceLeaderboardDto } from "@/app/races/[id]/definitions"
 import { Irp } from "@/app/results/[id]/definitions"
 import { DialogContent } from "@/components/ui/dialog"
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import { Info } from "lucide-react"
-import React, { useState } from "react"
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet"
+import { InfoIcon } from "lucide-react"
+import React, { useEffect, useState } from "react"
 
 export default function QuickViewDialogContent({
   apiHost,
@@ -18,23 +20,20 @@ export default function QuickViewDialogContent({
   leaderboard: RaceLeaderboardDto | null
   apiHost: string
 }) {
+  const [sheetOpen, setSheetOpen] = useState<boolean>(false)
   const [irpDetails, setIrpDetails] = useState<Irp | null>(null)
-  const [activeAthlete, setActiveAthlete] =
-    useState<LeaderboardResultDto | null>(null)
 
-  const handlePopoverChange = async (
-    open: boolean,
-    irp: LeaderboardResultDto,
-  ) => {
-    if (!open) {
-      return
+  useEffect(() => {
+    if (irpDetails) {
+      setSheetOpen(true)
     }
+  }, [irpDetails])
 
+  const getIrpData = async (irp: LeaderboardResultDto) => {
     const url = `${apiHost}/irpApi/${irp.athleteCourseId}`
     const response = await fetch(url)
     const result = (await response.json()) as Irp
-
-    // setIrpDetails(result)
+    setIrpDetails(result)
   }
 
   if (!leaderboard) {
@@ -79,52 +78,69 @@ export default function QuickViewDialogContent({
                   </tr>
                 </thead>
                 <tbody className="text-sm">
-                  {board.results.map((irp) => (
-                    <tr
-                      key={irp.athleteCourseId}
-                      className="border-b border-gray-300"
-                    >
-                      <td className="text-left py-2">
-                        <a href={`/results/${irp.athleteCourseId}`}>View</a>
-                      </td>
-                      <td className="py-2">
-                        <span
-                          className="bg-gray-800 text-white px-2 py-1 rounded"
-                          title="bib"
-                        >
-                          {irp.bib}
-                        </span>
-                      </td>
-                      <td className="py-2">
-                        <div>
-                          <a href={`/athletes/${irp.athleteId}`}>
-                            {irp.fullName}
-                          </a>
-                        </div>
-                        <div>
-                          {irp.genderAbbreviated} | {irp.raceAge}
-                        </div>
-                      </td>
-                      <td className="font-bold text-gray-500 py-2">
-                        {irp.overallRank}
-                      </td>
-                      <td className="font-bold text-gray-500 py-2">
-                        {irp.genderRank}
-                      </td>
-                      <td className="font-bold text-gray-500 py-2">
-                        {irp.divisionRank}
-                      </td>
-                      <td className="font-bold py-2">
-                        {irp.paceWithTimeCumulative.timeFormatted}
-                      </td>
-                      <td className="py-2">
-                        <div className="font-bold">
-                          {irp.paceWithTimeCumulative.paceValue || "--"}
-                        </div>
-                        {irp.paceWithTimeCumulative.paceLabel}
-                      </td>
-                    </tr>
-                  ))}
+                  {board.results.map((irp) => {
+                    return (
+                      <tr
+                        key={irp.athleteCourseId}
+                        className="border-b border-gray-300"
+                      >
+                        <td className="text-left py-2">
+                          <a href={`/results/${irp.athleteCourseId}`}>View</a>
+                          <div>
+                            <InfoIcon onClick={() => getIrpData(irp)} />
+                            <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+                              <SheetContent>
+                                <SheetHeader>
+                                  <SheetTitle>
+                                    Are you absolutely sure?
+                                  </SheetTitle>
+                                  <SheetDescription>
+                                    {irpDetails?.bib}
+                                  </SheetDescription>
+                                </SheetHeader>
+                              </SheetContent>
+                            </Sheet>
+                          </div>
+                        </td>
+                        <td className="py-2">
+                          <span
+                            className="bg-gray-800 text-white px-2 py-1 rounded"
+                            title="bib"
+                          >
+                            {irp.bib}
+                          </span>
+                        </td>
+                        <td className="py-2">
+                          <div>
+                            <a href={`/athletes/${irp.athleteId}`}>
+                              {irp.fullName}
+                            </a>
+                          </div>
+                          <div>
+                            {irp.genderAbbreviated} | {irp.raceAge}
+                          </div>
+                        </td>
+                        <td className="font-bold text-gray-500 py-2">
+                          {irp.overallRank}
+                        </td>
+                        <td className="font-bold text-gray-500 py-2">
+                          {irp.genderRank}
+                        </td>
+                        <td className="font-bold text-gray-500 py-2">
+                          {irp.divisionRank}
+                        </td>
+                        <td className="font-bold py-2">
+                          {irp.paceWithTimeCumulative.timeFormatted}
+                        </td>
+                        <td className="py-2">
+                          <div className="font-bold">
+                            {irp.paceWithTimeCumulative.paceValue || "--"}
+                          </div>
+                          {irp.paceWithTimeCumulative.paceLabel}
+                        </td>
+                      </tr>
+                    )
+                  })}
                 </tbody>
               </table>
             </div>
