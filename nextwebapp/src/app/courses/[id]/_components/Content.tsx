cb1bcd188216d8baa4007b6647eac5da1714db99
Justin Antranikian <justin.antranikian@gmail.com>
2024-12-06 22:43:21 -0700

    JA/added-result-compare

--- nextwebapp/src/app/courses/[id]/_components/Content.tsx
+++ nextwebapp/src/app/courses/[id]/_components/Content.tsx
@@ -43,24 +43,34 @@ export default function Content({
   }
 
   const ComparePane = () => {
+    if (hideComparePane) {
+      return (
+        <div className="fixed bottom-0 left-0 w-full bg-gray-200 bg-opacity-50 text-black px-4 text-right">
+          <span
+            onClick={() => setHideComparePane(false)}
+            className="cursor-pointer"
+          >
+            open
+          </span>
+        </div>
+      )
+    }
+
     return (
-      <div className="fixed bottom-0 left-0 py-5 w-full bg-gray-200 bg-opacity-50 text-black flex items-center justify-between px-4">
-        {hideComparePane ? (
-          <div className="text-right">
-            <span onClick={() => setHideComparePane(false)}>open</span>
-          </div>
-        ) : (
-          <>
-            <div className="text-center flex-1">
-              <a href={`/compare-results?ids=${idsEncoded}`}>
-                Compare ({selectedIds.length})
-              </a>
-            </div>
-            <div className="text-right">
-              <span onClick={() => setHideComparePane(true)}>x</span>
-            </div>
-          </>
-        )}
+      <div className="fixed bottom-0 left-0 py-3 w-full bg-gray-200 bg-opacity-50 text-black flex items-center justify-between px-4">
+        <div className="text-center flex-1">
+          <a href={`/compare-results?ids=${idsEncoded}`}>
+            Compare ({selectedIds.length})
+          </a>
+        </div>
+        <div className="text-right">
+          <span
+            className="cursor-pointer"
+            onClick={() => setHideComparePane(true)}
+          >
+            x
+          </span>
+        </div>
       </div>
     )
   }
