fef2313042bbc8529fed1fa2c12c0083ae385ae9
Justin Antranikian <justin.antranikian@gmail.com>
2024-12-11 18:55:06 -0700

    JA/updated-race-content.tsx

--- nextwebapp/src/app/races/[id]/_components/Content.tsx
+++ nextwebapp/src/app/races/[id]/_components/Content.tsx
@@ -9,6 +9,14 @@ import { Dialog } from "@/components/ui/dialog"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import IrpQuickView from "./IrpQuickView"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function Content({
   apiHost,
@@ -37,44 +45,26 @@ export default function Content({
           <div className="mb-8 text-purple-500 bold text-2xl">
             {leaderboard.courseName}
           </div>
-          <table className="table-auto w-full">
-            <thead className="text-lg">
-              <tr className="border-b border-black">
-                <th className="w-[5%] text-left py-2" scope="col"></th>
-                <th className="w-[5%] text-left py-2" scope="col"></th>
-                <th className="w-[15%] text-left py-2" scope="col">
-                  Bib
-                </th>
-                <th className="w-[20%] text-left py-2" scope="col">
-                  Name
-                </th>
-                <th className="w-[10%] text-left py-2" scope="col">
-                  Overall
-                </th>
-                <th className="w-[10%] text-left py-2" scope="col">
-                  Gender
-                </th>
-                <th className="w-[10%] text-left py-2" scope="col">
-                  Division
-                </th>
-                <th className="w-[15%] text-left py-2" scope="col">
-                  Time
-                </th>
-                <th className="w-[10%] text-left py-2" scope="col">
-                  Pace
-                </th>
-              </tr>
-            </thead>
-            <tbody className="text-sm">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead></TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Overall</TableHead>
+                <TableHead>Gender</TableHead>
+                <TableHead>Division</TableHead>
+                <TableHead>Time</TableHead>
+                <TableHead>Pace</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
               {leaderboard.results.map((irp) => (
-                <tr
-                  key={irp.athleteCourseId}
-                  className="border-b border-gray-300"
-                >
-                  <td className="text-left py-2">
+                <TableRow key={irp.athleteCourseId}>
+                  <TableCell>
                     <a href={`/results/${irp.athleteCourseId}`}>View</a>
-                  </td>
-                  <td className="py-2">
+                  </TableCell>
+                  <TableCell>
                     <InfoIcon
                       className="cursor-pointer"
                       size={14}
@@ -82,45 +72,31 @@ export default function Content({
                       strokeWidth={1.5}
                       onClick={() => handleQuickViewClicked(irp)}
                     />
-                  </td>
-                  <td className="py-2">
-                    <span
-                      className="bg-gray-800 text-white px-2 py-1 rounded"
-                      title="bib"
-                    >
-                      {irp.bib}
-                    </span>
-                  </td>
-                  <td className="py-2">
+                  </TableCell>
+                  <TableCell>
                     <div>
                       <a href={`/athletes/${irp.athleteId}`}>{irp.fullName}</a>
                     </div>
                     <div>
-                      {irp.genderAbbreviated} | {irp.raceAge}
+                      {irp.genderAbbreviated} | {irp.bib}
                     </div>
-                  </td>
-                  <td className="font-bold text-gray-500 py-2">
-                    {irp.overallRank}
-                  </td>
-                  <td className="font-bold text-gray-500 py-2">
-                    {irp.genderRank}
-                  </td>
-                  <td className="font-bold text-gray-500 py-2">
-                    {irp.divisionRank}
-                  </td>
-                  <td className="font-bold py-2">
+                  </TableCell>
+                  <TableCell>{irp.overallRank}</TableCell>
+                  <TableCell>{irp.genderRank}</TableCell>
+                  <TableCell>{irp.divisionRank}</TableCell>
+                  <TableCell className="font-bold">
                     {irp.paceWithTimeCumulative.timeFormatted}
-                  </td>
-                  <td className="py-2">
+                  </TableCell>
+                  <TableCell>
                     <div className="font-bold">
                       {irp.paceWithTimeCumulative.paceValue || "--"}
                     </div>
                     {irp.paceWithTimeCumulative.paceLabel}
-                  </td>
-                </tr>
+                  </TableCell>
+                </TableRow>
               ))}
-            </tbody>
-          </table>
+            </TableBody>
+          </Table>
           <div className="my-8 text-right">
             <Button>
               <Link href={`/courses/${leaderboard.courseId}`}>View</Link>
