ad118ed28914f5d246d277b7322deeed1dabc6f6
Justin Antranikian <justin.antranikian@gmail.com>
2025-01-01 11:15:23 -0700

    Updated directory dto

---
+++ Api/Orchestration/Locations/GetDirectory/DirectoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Orchestration.Locations.GetDirectory;
+
+public record DirectoryDto
+{
+    public required int Id { get; init; }
+
+    public required bool IsSelected { get; init; }
+    public required bool IsExpanded { get; set; }
+    public required string LocationType { get; init; }
+    public required string Name { get; init; }
+    public required string Slug { get; init; }
+    public required List<DirectoryDto> ChildLocations { get; init; }
+}
--- Api/Orchestration/Locations/GetDirectory/GetDirectoryOrchestrator.cs
+++ Api/Orchestration/Locations/GetDirectory/GetDirectoryOrchestrator.cs
@@ -3,18 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Orchestration.Locations.GetDirectory;
 
-public record DirectoryDto
-{
-    public required int Id { get; init; }
-
-    public required bool IsSelected { get; init; }
-    public required bool IsExpanded { get; set; }
-    public required string LocationType { get; init; }
-    public required string Name { get; init; }
-    public required string Slug { get; init; }
-    public required List<DirectoryDto> ChildLocations { get; init; }
-}
-
 public class GetDirectoryOrchestrator(ScoringDbContext dbContext)
 {
     public async Task<List<DirectoryDto>> Get(int? locationId)
