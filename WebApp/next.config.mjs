09d50f8841602d7f5633454b8186daee1f7086e6
Justin Antranikian <justin.antranikian@gmail.com>
2025-03-11 18:36:30 -0600

    Added-docker-file

--- WebApp/next.config.mjs
+++ WebApp/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/races",
-        permanent: false,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/races",
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
 }
 
 export default nextConfig
